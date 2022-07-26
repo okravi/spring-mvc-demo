1a452c65f70ca286e1efabb83b60726f97299670
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-07-26 15:07:59 +0300

    progress update

--- WebContent/WEB-INF/view/student-form.jsp
+++ WebContent/WEB-INF/view/student-form.jsp
@@ -9,11 +9,21 @@
 
 	<form:form action="processForm" modelAttribute="student">
 	
-		First Name: <form:input path="firstName"/>
-		<br><br>
-		Last Name: <form:input path="lastName"/>
-		<br><br>
-		<input type="submit" value="Submit"/>
+	First Name: <form:input path="firstName"/>
+	<br><br>
+	Last Name: <form:input path="lastName"/>
+	<br><br>
+	
+	Country:
+	
+	<form:select path="country">
+	
+		<form:options items="${student.countryOptions}"/>
+		
+	</form:select>
+	
+	<br><br>
+	<input type="submit" value="Submit"/>
 	
 	</form:form>
 
--- src/com/lc/springdemo/mvc/Student.java
+++ src/com/lc/springdemo/mvc/Student.java
@@ -1,11 +1,33 @@
 package com.lc.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
 		private String firstName;
 		private String lastName;
+		private String country;
+		private LinkedHashMap<String, String> countryOptions;
 		
-		public Student() {}
+		public Student() {
+			countryOptions = new LinkedHashMap<>();
+			countryOptions.put("BR", "Brazil");
+			countryOptions.put("FR", "France");
+			countryOptions.put("IN", "India");
+			countryOptions.put("US", "USA");
+		}
+		
+		public LinkedHashMap<String, String> getCountryOptions() {
+			return countryOptions;
+		}
+
+		public String getCountry() {
+			return country;
+		}
+
+		public void setCountry(String country) {
+			this.country = country;
+		}
 
 		public String getFirstName() {
 			return firstName;
