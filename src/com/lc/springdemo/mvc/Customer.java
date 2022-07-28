eb0ce9e87384ebeeec9a5a882c7b7d344334cd01
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-07-28 20:44:57 +0300

    progress update

--- src/com/lc/springdemo/mvc/Customer.java
+++ src/com/lc/springdemo/mvc/Customer.java
@@ -1,6 +1,9 @@
 package com.lc.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -10,7 +13,13 @@ public class Customer {
 	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
-
+	
+	@Min(value=0, message="must be more or equal than 0")
+	@Max(value=10, message="must be less or equal than 10")
+	private int freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
 	
 	public String getFirstName() {
 		return firstName;
@@ -27,6 +36,23 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
 	
 }
 
